63b363db4dcd510b39bb5f9c947b67fe5dacf7fe
yousra.mashkoor <yousra.mashkoor@tenpearls.com>
2021-01-14 22:19:46 +0500

    es6 practice

---
+++ create-a-module-script.js
@@ -0,0 +1,8 @@
+<html>
+  <script type="module" src="index.js"></script>
+  <body>
+    <!-- Only change code below this line -->
+
+    <!-- Only change code above this line -->
+  </body>
+</html>
